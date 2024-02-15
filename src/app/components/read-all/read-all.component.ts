717ff235d3f6f9e31c912765082a05b4a889d0e5
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2024-02-14 23:07:22 -0300

    Created List and Components in the read all

--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Todo } from '../../models/todo';
 
 @Component({
   selector: 'app-read-all',
   templateUrl: './read-all.component.html',
   styleUrl: './read-all.component.css'
 })
-export class ReadAllComponent {
+export class ReadAllComponent implements OnInit {
+    list: Todo[] =[
+      {
+        titulo:"Teste",
+        dataParaFinalizar: new Date,
+        finalizado:false
+      },
+      {
+        titulo:"Teste 2",
+        dataParaFinalizar: new Date,
+        finalizado:false
+      }
+    ]
+    
+    constructor(){}
+
+    ngOnInit(): void {
+    }
 
 }
