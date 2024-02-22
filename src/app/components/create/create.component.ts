268352a64a6de58702952c625c65f6b99f7119e5
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2024-02-22 17:23:56 -0300

    Correction of the list of correlated categories and routerLink create

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  cancelar(): void {
+    this.router.navigate(['']);
+  }
 
 }
